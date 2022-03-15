09c0e79a669faba0b108a36cf2692221ccff202f
Vinizada <vinicius.lima@diferpan.com.br>
2022-03-14 23:05:48 -0300

    Primeiro commit, apenas com o ambiente de desenvolvimento

---
+++ src/main/java/com/vinicius/springbootapirest/SpringbootapirestApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.springbootapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapirestApplication.class, args);
+	}
+
+}
